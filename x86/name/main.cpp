ef4a1971de41d175f1a6ad588706c31472c41b80
Patrix9999 <patrixon9999@wp.pl>
2026-04-06 20:25:19 +0200

    Added `x86/name`

---
+++ x86/name/main.cpp
@@ -0,0 +1,22 @@
+// C++ 23 example
+
+#include <string>
+#include <string_view>
+
+#include <format>
+#include <print>
+
+inline std::string format_rgb(unsigned char r, unsigned char g, unsigned char b, std::string_view text)
+{
+    static constexpr std::string_view reset = "\033[0m";
+    return std::format("\033[38;2;{};{};{}m{}{}", r, g, b, text, reset);
+}
+
+int main(int argc, char* argv[])
+{
+    std::println("{} {} {}({})", format_rgb(210, 140, 110, "void"), format_rgb(210, 140, 110, "__cdecl"), format_rgb(140, 140, 40, "func"), format_rgb(210, 140, 110, "void"));
+    std::println("{}: ?{}@@YAXXZ", format_rgb(80, 160, 210, "mangled"), format_rgb(140, 140, 40, "func"));
+    std::println();
+
+    return 0;
+}
