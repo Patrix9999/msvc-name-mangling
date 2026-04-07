4e385d7e0675eb999f78925d55304b43b749bdf4
Patrix9999 <patrixon9999@wp.pl>
2026-04-07 22:20:27 +0200

    Corrected `x86/method/args`

--- x86/method/args/main.cpp
+++ x86/method/args/main.cpp
@@ -15,19 +15,19 @@ inline std::string format_rgb(unsigned char r, unsigned char g, unsigned char b,
 int main(int argc, char* argv[])
 {
     std::println("{}: {} {} {}::method_no_args({})", format_rgb(210, 140, 110, "public"), format_rgb(210, 140, 110, "void"), format_rgb(210, 140, 110, "__thiscall"), format_rgb(180, 180, 210, "cls"), format_rgb(210, 140, 110, "void"));
-    std::println("{}: ?method_no_args@Cls@@QAEX{}Z", format_rgb(80, 160, 210, "mangled"), format_rgb(140, 140, 40, "X"));
+    std::println("{}: ?method_no_args@cls@@QAEX{}Z", format_rgb(80, 160, 210, "mangled"), format_rgb(140, 140, 40, "X"));
     std::println();
 
     std::println("{}: {} {} {}::method_one_arg({})", format_rgb(210, 140, 110, "public"), format_rgb(210, 140, 110, "void"), format_rgb(210, 140, 110, "__thiscall"), format_rgb(180, 180, 210, "cls"), format_rgb(210, 140, 110, "void"));
-    std::println("{}: ?method_one_arg@Cls@@QAEX{}{}Z", format_rgb(80, 160, 210, "mangled"), format_rgb(140, 140, 40, "H"), format_rgb(180, 40, 40, "@"));
+    std::println("{}: ?method_one_arg@cls@@QAEX{}{}Z", format_rgb(80, 160, 210, "mangled"), format_rgb(140, 140, 40, "H"), format_rgb(180, 40, 40, "@"));
     std::println();
 
     std::println("{}: {} {} {}::method_two_args({})", format_rgb(210, 140, 110, "public"), format_rgb(210, 140, 110, "void"), format_rgb(210, 140, 110, "__thiscall"), format_rgb(180, 180, 210, "cls"), format_rgb(210, 140, 110, "void"));
-    std::println("{}: ?method_two_args@Cls@@QAEX{}{}Z", format_rgb(80, 160, 210, "mangled"), format_rgb(140, 140, 40, "HH"), format_rgb(180, 40, 40, "@"));
+    std::println("{}: ?method_two_args@cls@@QAEX{}{}Z", format_rgb(80, 160, 210, "mangled"), format_rgb(140, 140, 40, "HH"), format_rgb(180, 40, 40, "@"));
     std::println();
 
     std::println("{}: {} {} {}::method_three_args({})", format_rgb(210, 140, 110, "public"), format_rgb(210, 140, 110, "void"), format_rgb(210, 140, 110, "__thiscall"), format_rgb(180, 180, 210, "cls"), format_rgb(210, 140, 110, "void"));
-    std::println("{}: ?method_three_args@Cls@@QAEX{}{}Z", format_rgb(80, 160, 210, "mangled"), format_rgb(140, 140, 40, "HHH"), format_rgb(180, 40, 40, "@"));
+    std::println("{}: ?method_three_args@cls@@QAEX{}{}Z", format_rgb(80, 160, 210, "mangled"), format_rgb(140, 140, 40, "HHH"), format_rgb(180, 40, 40, "@"));
     std::println();
 
     return 0;
